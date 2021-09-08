0e1cea77058ffdd3d2f70cca7a8156675ee0b0f0
pelow22 <peter.lowenberg.neto@gmail.com>
2021-09-08 16:43:21 -0300

    Análise com os gêneros

--- Kruskal-Wallis.R
+++ Kruskal-Wallis.R
@@ -6,6 +6,8 @@ library(ggpubr)
 #carregar a tabela
 #Dados de acidentes ofídicos no Paraná organizados pela Laura.
 
+
+
 my_data <- read.csv(file = "data_subset.csv", sep = ";", header = TRUE)
 
 #checar a tabela
@@ -82,3 +84,91 @@ pairwise.wilcox.test(my_data2$Total, my_data2$veg,
 
 #Formações diferentes quanto: EST x FES; FES x FOD; FES x FOM; FES x SAV
 
+
+############################Bothrops
+my_data2
+
+group_by(my_data2, veg) %>%
+    summarise(
+        count = n(),
+        mean = mean(Bothrops, na.rm = TRUE),
+        sd = sd(Bothrops, na.rm = TRUE),
+        median = median(Bothrops, na.rm = TRUE),
+        IQR = IQR(Bothrops, na.rm = TRUE)
+    )
+
+ggboxplot(my_data2, x = "veg", y = "Bothrops",
+          color = "veg", palette = c("#00AFBB", "#E7B800", "#FC4E07", "#00AFBB", "#E7B800", "#FC4E07"),
+          order = c("Floresta Estacional Semidecidual", "Savana", "Floresta Ombrofila Mista", "Floresta Ombrofila Densa", "Estepe" ),
+          ylab = "Acidentes ofídicos (Bothrops)", xlab = "Formação Vegetal")
+
+ggline(my_data2, x = "veg", y = "Bothrops",
+       add = c("mean_se", "jitter"),
+       order = c("Floresta Estacional Semidecidual", "Savana", "Floresta Ombrofila Mista", "Floresta Ombrofila Densa", "Estepe" ),
+       ylab = "Acidentes ofídicos (Bothrops)", xlab = "Formação Vegetal")
+
+kruskal.test(Bothrops ~ veg, data = my_data2)
+
+#Kruskal-Wallis rank sum test
+
+#data:  Bothrops by veg
+#Kruskal-Wallis chi-squared = 102.26, df = 4, p-value < 2.2e-16
+
+pairwise.wilcox.test(my_data2$Bothrops, my_data2$veg,
+                     p.adjust.method = "none")
+
+#Pairwise comparisons using Wilcoxon rank sum test with continuity correction
+
+#data:  my_data2$Bothrops and my_data2$veg
+
+#                                 Estepe  Floresta Estacional Semidecidual Floresta Ombrofila Densa Floresta Ombrofila Mista
+#Floresta Estacional Semidecidual 0.00067 -                                -                        -
+#Floresta Ombrofila Densa         0.79720 0.00051                          -                        -
+#Floresta Ombrofila Mista         0.20849 < 2e-16                          0.13398                  -
+#Savana                           0.65868 5.4e-05                          0.31624                  0.56305
+#P value adjustment method: none
+
+
+############################Crotalus
+my_data2
+
+group_by(my_data2, veg) %>%
+    summarise(
+        count = n(),
+        mean = mean(Crotalus, na.rm = TRUE),
+        sd = sd(Crotalus, na.rm = TRUE),
+        median = median(Crotalus, na.rm = TRUE),
+        IQR = IQR(Crotalus, na.rm = TRUE)
+    )
+
+ggboxplot(my_data2, x = "veg", y = "Crotalus",
+          color = "veg", palette = c("#00AFBB", "#E7B800", "#FC4E07", "#00AFBB", "#E7B800", "#FC4E07"),
+          order = c("Floresta Estacional Semidecidual", "Savana", "Floresta Ombrofila Mista", "Floresta Ombrofila Densa", "Estepe" ),
+          ylab = "Acidentes ofídicos (Crotalus)", xlab = "Formação Vegetal")
+
+ggline(my_data2, x = "veg", y = "Crotalus",
+       add = c("mean_se", "jitter"),
+       order = c("Floresta Estacional Semidecidual", "Savana", "Floresta Ombrofila Mista", "Floresta Ombrofila Densa", "Estepe" ),
+       ylab = "Acidentes ofídicos (Crotalus)", xlab = "Formação Vegetal")
+
+kruskal.test(Crotalus ~ veg, data = my_data2)
+
+#Kruskal-Wallis rank sum test
+
+#data:  Crotalus by veg
+#Kruskal-Wallis chi-squared = 12.056, df = 4, p-value = 0.01694
+
+pairwise.wilcox.test(my_data2$Crotalus, my_data2$veg,
+                     p.adjust.method = "none")
+
+#Pairwise comparisons using Wilcoxon rank sum test with continuity correction
+
+#data:  my_data2$Crotalus and my_data2$veg
+
+#                                 Estepe Floresta Estacional Semidecidual Floresta Ombrofila Densa Floresta Ombrofila Mista
+#Floresta Estacional Semidecidual 0.0530 -                                -                        -
+#Floresta Ombrofila Densa         0.4993 0.9775                           -                        -
+#Floresta Ombrofila Mista         0.0338 0.4136                           0.9365                   -
+#Savana                           0.1953 0.0058                           0.3114                   0.0077
+
+#P value adjustment method: none
